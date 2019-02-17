4377d44ba1ccfff5b3adbc66a5c1e8f82fbd491c
Rakesh Katta <rakkatta@cisco.com>
2019-02-17 14:54:10 +0000

    helloworld

---
+++ sample.java
@@ -0,0 +1,17 @@
+public class sample {
+
+
+// public =  AcessModifier
+// static  = able to execute a method that isn't part of a class definition
+// void =  Return value is void 
+// main =  Main Method Name
+// String [] =  Strings.
+	public static void main(String[] args) {
+		for(int i=1;i<=10;i++) {
+		System.out.println("Hello World");
+		System.out.println("Ramkishan \n Saraaaa this is my first JAVA_Program");
+		// TODO Auto-generated method stub
+	    }
+	}
+
+}
